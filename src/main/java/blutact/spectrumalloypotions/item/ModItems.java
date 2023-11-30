ae7e5fecc670291f07e05998c4501c4d297849e5
blutact <elo51297@gmail.com>
2023-11-30 10:54:22 +0100

    added final product of bedrockium

--- src/main/java/blutact/spectrumalloypotions/item/ModItems.java
+++ src/main/java/blutact/spectrumalloypotions/item/ModItems.java
@@ -2,8 +2,12 @@ package blutact.spectrumalloypotions.item;
 
 import blutact.spectrumalloypotions.SpectrumAlloyPotions;
 import blutact.spectrumalloypotions.block.ModBlocks;
+import de.dafuqs.spectrum.registries.SpectrumStatusEffects;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -12,6 +16,16 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     public static final Item BEDROCKIUM_CRYSTAL = registerItem("bedrockium_crystal",
             new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
+
+    public static final Item BEDROCKIUM_CANDY = registerItem("bedrockium_candy",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().alwaysEdible()
+                    .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200, 10), 1.0F)
+                    .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 250, 4), 1.0F)
+                    .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 200, 10), 1.0F)
+                    .statusEffect(new StatusEffectInstance(SpectrumStatusEffects.DENSITY, 200, 20), 1.0F)
+                    .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 100, 100), 1.0F)
+                    .build())));;
+
     public static final Item BEDROCKIUM_CRYSTAL_SEEDS = registerItem("bedrockium_crystal_seeds",
             new AliasedBlockItem(ModBlocks.BEDROCKIUM_CRYSTAL_BLOCK
                     ,new FabricItemSettings().group(ItemGroup.MISC)));
